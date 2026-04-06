0b2127a71d43a00ebbc9faf495bba0fe73b2e921
Ethan Bennett <ebennett19@gmail.com>
2026-04-06 14:12:17 -0400

    Add keystore import script, password prompt on local startup, and keystore decryption

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { setUpCrashHandlers } from './utils/logger';
+import { initClient } from './utils/client';
 import { startScheduler } from './utils/scheduler';
 
 setUpCrashHandlers();
+await initClient();
 startScheduler();
--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,11 +1,15 @@
 import 'dotenv/config';
 
-const REQUIRED = ['RPC_URL', 'PRIVATE_KEY', 'SUBGRAPH_URL'] as const;
+const REQUIRED = ['RPC_URL', 'SUBGRAPH_URL'] as const;
 
 for (const key of REQUIRED) {
   if (!process.env[key]) {
     throw new Error(`${key} must be specified`);
   }
+}
+
+if (!process.env.PRIVATE_KEY && !process.env.KEYSTORE_PATH) {
+  throw new Error('Either PRIVATE_KEY or KEYSTORE_PATH must be specified');
 }
 
 if (process.env.ORACLE_API_KEY && !process.env.ORACLE_API_TIER) {
@@ -14,7 +18,8 @@ if (process.env.ORACLE_API_KEY && !process.env.ORACLE_API_TIER) {
 
 export const env = {
   RPC_URL: process.env.RPC_URL!,
-  PRIVATE_KEY: process.env.PRIVATE_KEY!,
+  PRIVATE_KEY: process.env.PRIVATE_KEY,
+  KEYSTORE_PATH: process.env.KEYSTORE_PATH,
   SUBGRAPH_URL: process.env.SUBGRAPH_URL!,
   ORACLE_API_KEY: process.env.ORACLE_API_KEY,
   ORACLE_API_TIER: process.env.ORACLE_API_TIER,
